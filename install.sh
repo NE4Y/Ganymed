849f0a32af7da105892a59f748a5c5268a453819
Steffen Lindner <Steffen@Steffens-MBP.fritz.box>
2016-01-08 16:07:32 +0100

    Added installer

--- install.sh
+++ install.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
+echo "Install needed ruby modules"
+
 gem install colorize json ipaddress 
+
+echo "Init git repo for update"
+
+git init && git remote add origin https://github.com/NE4Y/Ganymed
+
+echo "Move startscript"
+
+cp gnm /usr/local/bin/gnm && chmod a+x /usr/local/bin/gnm
+
+echo "Copy files"
+
+cp -R ../Ganymed /usr/local/ 2> /dev/null
