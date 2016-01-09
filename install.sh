43b344fba98ff5739a2073356f447cf79edd8d37
NE4Y <Steffen@Steffens-MBP.fritz.box>
2016-01-09 14:08:57 +0100

    Added colored output

--- install.sh
+++ install.sh
@@ -2,6 +2,11 @@
 
 INSTALL_DIR=/usr/local
 
+# Colors
+NC='\033[0m'
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+
 # check for need ruby modules
 for MOD in colorize json ipaddress
 do
@@ -16,19 +21,22 @@ done
 echo "Test if installation dir is writeable"
 
 if ! [ -w ${INSTALL_DIR} ]; then
-  echo "${INSTALL_DIR} not writeable" && exit 1
+  echo -e "${RED}[error] ${NC} ${INSTALL_DIR} not writeable" && exit 1
+else
+  echo -e "${GREEN}[ok] ${NC} ${INSTALL_DIR} is writeable"
 fi
  
 echo "Link startscript"
 
 rm ${INSTALL_DIR}/bin/gnm > /dev/null
-ln -s ${INSTALL_DIR}/Ganymed/gnm ${INSTALL_DIR}/bin/gnm 
+ln -s ${INSTALL_DIR}/Ganymed/gnm ${INSTALL_DIR}/bin/gnm && echo -e "${GREEN}[ok] ${NC} Startscript linked"
 
 echo "Copy files"
 
 rm -rf ${INSTALL_DIR}/Ganymed > /dev/null
 rsync -az --exclude .git ../Ganymed/ ${INSTALL_DIR}/Ganymed
 
+echo -e "${GREEN}[ok] ${NC} Files copied"
 
 echo "Init git repo for update"
 
